3f58bb4a92ad9c7df1b42fb8f1c3c50624993a73
Claudieta95 <cmaldonadoq@uoc.edu>
2026-04-08 12:08:43 +0200

    Añadidos layouts compartidos

---
+++ app/views/layouts/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/home">Inicio</a></li>
+        <li><a href="/services">Servicios</a></li>
+        <li><a href="/client/dashboard">Panel cliente</a></li>
+        <li><a href="/auth/login">Login</a></li>
+    </ul>
+</nav>
