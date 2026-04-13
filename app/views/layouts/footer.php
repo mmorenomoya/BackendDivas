006b4b5414dded2ba674b42104182ea742c89767
Andrea Ros <androsrivas@outlook.com>
2026-04-13 20:45:24 +0200

    fix global css styling

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,8 +1,6 @@
-    <footer class="site-footer">
-        <div class="site-footer__inner">
-            <span>© 2026 ReparaYA - Backend Divas</span>
-            <span>Gestión de reparaciones domésticas</span>
-        </div>
-    </footer>
-</body>
-</html>
+<footer class="site-footer">
+    <div class="site-footer__inner">
+        <span>© 2026 ReparaYA - Backend Divas</span>
+        <span>Gestión de reparaciones domésticas</span>
+    </div>
+</footer>
